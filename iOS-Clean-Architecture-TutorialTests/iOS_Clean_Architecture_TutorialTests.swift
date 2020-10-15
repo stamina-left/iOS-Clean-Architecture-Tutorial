9353c5d3562cf2bb118bbcc0d2fc7f9afec78fdb
Albert Pangestu <stamina.left@gmail.com>
2020-10-15 13:39:00 +0700

    Initial Commit

---
+++ iOS-Clean-Architecture-TutorialTests/iOS_Clean_Architecture_TutorialTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_Clean_Architecture_TutorialTests.swift
+//  iOS-Clean-Architecture-TutorialTests
+//
+//  Created by Albert Pangestu on 15/10/20.
+//
+
+import XCTest
+@testable import iOS_Clean_Architecture_Tutorial
+
+class iOS_Clean_Architecture_TutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
