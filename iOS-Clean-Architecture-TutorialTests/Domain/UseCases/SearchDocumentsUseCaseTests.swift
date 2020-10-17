ba8937d8ebef79a6e79a19f0a93d40a473294cdd
Albert Pangestu <stamina.left@gmail.com>
2020-10-17 19:53:36 +0700

    Change unit test name and added a blank search documents use case unit test.

--- iOS-Clean-Architecture-TutorialTests/Domain/UseCases/SearchDocumentsUseCaseTests.swift
+++ iOS-Clean-Architecture-TutorialTests/Domain/UseCases/SearchDocumentsUseCaseTests.swift
@@ -19,42 +19,10 @@ class SearchDocumentsUseCaseTests: XCTestCase {
     func testSearchDocumentsUseCase_whenSuccessfullySaveDocument_thenFetchesDocuments() {
         
         // given
-        let expectation = self.expectation(description: "Recent document saved.")
-        expectation.expectedFulfillmentCount = 2
-        let coreDataStorage = CoreDataStorage()
-        let storage = DefaultDocumentsStorage(coreDataStorage: coreDataStorage)
-        let repository = DefaultDocumentsRepository(storage: storage)
-        let searchDocumentsUseCase = DefaultSearchDocumentsUseCase(repository: repository)
-        let saveDocumentUseCase = DefaultSaveDocumentUseCase(repository: repository)
-        var resultDocument: Document = SearchDocumentsUseCaseTests.documents[2]
         
         // when
-        let responseDTO = SaveDocumentUseCaseResponseDTO(document: SearchDocumentsUseCaseTests.documents[0])
-        saveDocumentUseCase.execute(responseDTO: responseDTO, completion: { result in
-            switch result {
-            case .success(let document):
-                resultDocument = Document(id: document.id, title: document.title, content: document.content, created: document.created, modified: document.modified)
-                expectation.fulfill()
-            case .failure(let error):
-                XCTFail()
-                print(error.localizedDescription)
-            }
-        })
         
         //then
-        var recentDocuments = [Document]()
-        let requestDTO = SearchDocumentsUseCaseRequestDTO(name: "American Psycho")
-        searchDocumentsUseCase.execute(requestDTO: requestDTO, completion: { result in
-            switch result {
-            case .success(let documents):
-                recentDocuments = documents
-                expectation.fulfill()
-            case .failure(let error):
-                XCTFail()
-                print(error.localizedDescription)
-            }
-        })
-        waitForExpectations(timeout: 5, handler: nil)
-        XCTAssertTrue(recentDocuments.contains(resultDocument))
+        
     }
 }
